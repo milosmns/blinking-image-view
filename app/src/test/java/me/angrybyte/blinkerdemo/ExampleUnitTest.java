92075f92198210359764a278c2b6f3b41476026e
Milos Marinkovic <milosmns@gmail.com>
2018-04-25 16:53:55 +0200

    Initial cleanup

--- app/src/test/java/me/angrybyte/blinkerdemo/ExampleUnitTest.java
+++ app/src/test/java/me/angrybyte/blinkerdemo/ExampleUnitTest.java
@@ -2,17 +2,13 @@ package me.angrybyte.blinkerdemo;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class ExampleUnitTest {
 
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
 }
