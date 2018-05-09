d10cee49cd299753ba367cc048e8c8fc2d2d184e
Milos Marinkovic <milosmns@gmail.com>
2018-05-09 18:21:46 +0200

    Updating Blinker to get all features and using it for real

--- app/src/main/java/me/angrybyte/blinkerdemo/MainActivity.java
+++ app/src/main/java/me/angrybyte/blinkerdemo/MainActivity.java
@@ -3,12 +3,23 @@ package me.angrybyte.blinkerdemo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import me.angrybyte.blinkerview.BlinkerView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.textView).setOnClickListener(v -> {
+            final BlinkerView blinker = findViewById(R.id.blinkerView_manual);
+            if (blinker.isBlinkingOn()) {
+                blinker.stopBlinking();
+            } else {
+                blinker.startBlinking();
+            }
+        });
     }
 
 }
--- blinkerview/src/main/java/me/angrybyte/blinkerview/BlinkerView.java
+++ blinkerview/src/main/java/me/angrybyte/blinkerview/BlinkerView.java
@@ -1,5 +1,8 @@
 package me.angrybyte.blinkerview;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.res.TypedArray;
@@ -15,9 +18,12 @@ import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
 
 import java.lang.annotation.Retention;
 
+import static android.animation.ValueAnimator.AnimatorListener;
+import static android.animation.ValueAnimator.AnimatorUpdateListener;
 import static java.lang.annotation.RetentionPolicy.SOURCE;
 
 /**
@@ -26,30 +32,35 @@ import static java.lang.annotation.RetentionPolicy.SOURCE;
  */
 public class BlinkerView extends View {
 
-    public static final int SCALE_STRETCH = 0;
-    public static final int SCALE_CONSTRAIN = 1;
-    public static final int SCALE_CENTER = 2;
-
-    private static final String TAG = BlinkerView.class.getSimpleName();
-    private static final int DEF_INTERVAL = 500; // ms
+    private static String TAG = BlinkerView.class.getSimpleName();
     private static final String KEY_SCALE_TYPE = "blinker_scale_type";
     private static final String KEY_DURATION = "blinker_duration";
     private static final String KEY_FADE = "blinker_fade";
     private static final String KEY_BLINK = "blinker_should_blink";
     private static final String KEY_SUPER_STATE = "blinker_super_state";
+    private static final int DEF_INTERVAL = 500; // ms
+
+    // <editor-fold desc="Scale types">
+    public static final int SCALE_STRETCH = 0;
+    public static final int SCALE_CONSTRAIN = 1;
+    public static final int SCALE_CENTER = 2;
 
     @Retention(SOURCE)
-    @IntDef({SCALE_STRETCH, SCALE_CONSTRAIN, SCALE_CENTER})
+    @IntDef({ SCALE_STRETCH, SCALE_CONSTRAIN, SCALE_CENTER })
     public @interface ScaleType {}
+    // </editor-fold>
 
     @Nullable
     private Drawable mDrawable;
+    @NonNull
+    private Rect mDrawableBounds = new Rect();
+    @NonNull
+    private ValueAnimator mAnimator = new ValueAnimator();
     @ScaleType
     private int mScaleType = SCALE_STRETCH;
     private int mInterval = DEF_INTERVAL;
-    private boolean mFade = true;
-    private Rect mDrawableBounds = new Rect();
     private boolean mShouldBlink = false;
+    private boolean mFade = true;
 
     // <editor-fold desc="Constructors">
     public BlinkerView(final @NonNull Context context) {
@@ -77,14 +88,29 @@ public class BlinkerView extends View {
 
     private void init(final @NonNull Context context, @Nullable final AttributeSet attrs, final int defStyleAttr, final int defStyleRes) {
         final TypedArray attributes = context.obtainStyledAttributes(attrs, R.styleable.BlinkerView, defStyleAttr, defStyleRes);
-
         mDrawable = attributes.getDrawable(R.styleable.BlinkerView_blink_drawable);
         mScaleType = attributes.getInteger(R.styleable.BlinkerView_blink_scale_type, mScaleType);
         mInterval = attributes.getInteger(R.styleable.BlinkerView_blink_interval, mInterval);
-        mFade = attributes.getBoolean(R.styleable.BlinkerView_blink_use_fading, mFade);
         mShouldBlink = attributes.getBoolean(R.styleable.BlinkerView_blink_autostart, false);
-
+        mFade = attributes.getBoolean(R.styleable.BlinkerView_blink_use_fading, mFade);
         attributes.recycle();
+        createAnimator();
+    }
+
+    /* Android integration */
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        if (mShouldBlink) {
+            startBlinking();
+        }
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        stopBlinking(false);
     }
 
     @Nullable
@@ -115,32 +141,6 @@ public class BlinkerView extends View {
         }
     }
 
-    @ScaleType
-    @SuppressWarnings("unused")
-    public int getScaleType() {
-        return mScaleType;
-    }
-
-    @SuppressWarnings("unused")
-    public void setScaleType(@ScaleType final int scaleType) {
-        mScaleType = scaleType;
-        updateDrawableBounds();
-        invalidate();
-    }
-
-    @Nullable
-    @SuppressWarnings("unused")
-    public Drawable getDrawable() {
-        return mDrawable;
-    }
-
-    @SuppressWarnings("unused")
-    public void setDrawable(@Nullable final Drawable drawable) {
-        mDrawable = drawable;
-        updateDrawableBounds();
-        invalidate();
-    }
-
     @Override
     public void setPadding(final int left, final int top, final int right, final int bottom) {
         super.setPadding(left, top, right, bottom);
@@ -153,30 +153,6 @@ public class BlinkerView extends View {
         super.setPaddingRelative(start, top, end, bottom);
         updateDrawableBounds();
         invalidate();
-    }
-
-    @SuppressWarnings("unused")
-    public void startBlinking() {
-        mShouldBlink = true;
-    }
-
-    @SuppressWarnings("unused")
-    public void stopBlinking() {
-        stopBlinking(true);
-    }
-
-    private void stopBlinking(boolean setFlag) {
-        if (setFlag) {
-            mShouldBlink = false;
-        }
-    }
-
-    @Override
-    protected void onAttachedToWindow() {
-        super.onAttachedToWindow();
-        if (mShouldBlink) {
-            startBlinking();
-        }
     }
 
     @Override
@@ -196,31 +172,148 @@ public class BlinkerView extends View {
         super.onDraw(canvas);
         if (mDrawable != null) {
             if (isInEditMode()) {
-                mDrawable.draw(canvas);
+                mDrawable.setAlpha(255);
+            } else if (mShouldBlink) {
+                int alpha = Math.round(mAnimator.getAnimatedFraction() * 255);
+                if (!mFade) {
+                    alpha = alpha > 255 / 2 ? 255 : 0;
+                }
+
+                mDrawable.setAlpha(alpha);
+            } else {
+                mDrawable.setAlpha(255);
             }
+            mDrawable.draw(canvas);
         }
     }
 
-    @Override
-    protected void onDetachedFromWindow() {
-        super.onDetachedFromWindow();
-        stopBlinking(false);
+    /* Public API */
+
+    /**
+     * Gets the current drawable's scale type. Always is one of {@link ScaleType}.
+     *
+     * @return The current scale type
+     */
+    @ScaleType
+    @SuppressWarnings("unused")
+    public int getScaleType() {
+        return mScaleType;
+    }
+
+    /**
+     * Sets the drawable's scale type. Must be one of {@link ScaleType}.
+     *
+     * @param scaleType The new scale type
+     */
+    @SuppressWarnings("unused")
+    public void setScaleType(@ScaleType final int scaleType) {
+        mScaleType = scaleType;
+        updateDrawableBounds();
+        invalidate();
+    }
+
+    /**
+     * Gets the blinker drawable, if any.
+     *
+     * @return A drawable that is currently set to blink, or {@code null} if no drawable is set
+     */
+    @Nullable
+    @SuppressWarnings("unused")
+    public Drawable getDrawable() {
+        return mDrawable;
+    }
+
+    /**
+     * Changes the blinker drawable.
+     *
+     * @param drawable Which drawable to blink now, set to {@code null} to make it invisible
+     */
+    @SuppressWarnings("unused")
+    public void setDrawable(@Nullable final Drawable drawable) {
+        mDrawable = drawable;
+        updateDrawableBounds();
+        invalidate();
+    }
+
+    /**
+     * Starts blinking the source drawable (infinitely).
+     */
+    @SuppressWarnings("unused")
+    public void startBlinking() {
+        mShouldBlink = true;
+        stopAnimator();
+        createAnimator().start();
+    }
+
+    /**
+     * Stops blinking the source drawable and returns it's alpha to 100%.
+     */
+    @SuppressWarnings("unused")
+    public void stopBlinking() {
+        stopBlinking(true);
+    }
+
+    /**
+     * Checks if the BlinkerView is set to blink. This doesn't mean that it's actually
+     * blinking, as blinking is disabled when detached from window.
+     *
+     * @return {@code True} if blinker is set to blink when attached to window, {@code false} if not
+     */
+    public boolean isBlinkingOn() {
+        return mShouldBlink;
     }
 
     /* Private helpers */
 
+    private void stopBlinking(boolean setFlag) {
+        if (setFlag) {
+            mShouldBlink = false;
+        }
+        stopAnimator();
+    }
+
+    private final AnimatorUpdateListener mUpdateListener = (animation) -> invalidate();
+
+    // @formatter:off
+    private final AnimatorListener mAnimationListener = new AnimatorListenerAdapter() {
+        @Override public void onAnimationEnd(Animator animation) { invalidate(); }
+        @Override public void onAnimationCancel(Animator animation) { invalidate(); }
+    };
+    // @formatter:on
+
+    @NonNull
+    private ValueAnimator createAnimator() {
+        final ValueAnimator animator = ValueAnimator.ofFloat(0.0f, 1.0f);
+        animator.setDuration(mInterval);
+        animator.setRepeatMode(ValueAnimator.REVERSE);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setInterpolator(new AccelerateDecelerateInterpolator());
+        animator.addUpdateListener(mUpdateListener);
+        animator.addListener(mAnimationListener);
+        mAnimator = animator;
+        return animator;
+    }
+
+    private void stopAnimator() {
+        mAnimator.cancel();
+        mAnimator.removeUpdateListener(mUpdateListener);
+        mAnimator.removeListener(mAnimationListener);
+    }
+
     @NonNull
     private Rect getPadding() {
+        // @formatter:off
         return new Rect(
-                Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1 ? getPaddingStart() : getPaddingLeft(),
-                getPaddingTop(),
-                Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1 ? getPaddingEnd() : getPaddingRight(),
-                getPaddingBottom()
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1 ? getPaddingStart() : getPaddingLeft(),
+            getPaddingTop(),
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1 ? getPaddingEnd() : getPaddingRight(),
+            getPaddingBottom()
         );
+        // @formatter:on
     }
 
     private void updateDrawableBounds() {
-        if (mDrawable == null) return;
+        if (mDrawable == null) { return; }
 
         final int intrinsicW, intrinsicH;
         switch (mScaleType) {
@@ -254,6 +347,7 @@ public class BlinkerView extends View {
         mDrawable.setBounds(mDrawableBounds);
     }
 
+    // <editor-fold desc="Applying drawable bounds">
     private void applyStretchBounds() {
         final Rect padding = getPadding();
         mDrawableBounds.left = padding.left;
@@ -291,5 +385,6 @@ public class BlinkerView extends View {
         mDrawableBounds.right = getWidth() / 2 + intrinsicW / 2;
         mDrawableBounds.bottom = getHeight() / 2 + intrinsicH / 2;
     }
+    // </editor-fold>
 
 }
